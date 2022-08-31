e91254a9f2dd1fe1ce8dba4ae39d8b0f091bd549
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-08-31 11:00:51 +0300

    country search options updated

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,19 +1,12 @@
 const SEARCH_COUNTRIES_URL = 'https://restcountries.com/v3.1/name/';
 
-const countrySearchOptions = {
-  countryName: 'name',
-  capital: 'capital',
-  population: 'population',
-  languages: 'languages',
-  flags: 'flags',
-};
+const countrySearchOptions = new URLSearchParams({
+  fields: 'name,capital,population,languages,flags',
+});
 
 export function fetchCountries(country) {
-  const { countryName, capital, population, languages, flags } =
-    countrySearchOptions;
-
   return fetch(
-    `${SEARCH_COUNTRIES_URL}${country}?fields=${countryName},${capital},${population},${languages},${flags}`
+    `${SEARCH_COUNTRIES_URL}${country}?${countrySearchOptions}`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
